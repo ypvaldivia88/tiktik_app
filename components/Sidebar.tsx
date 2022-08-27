6aa40f7202e8399b4ed9093fb1af78b6ebf37418
Yasmani Palmero Valdivia <yasmany.07@gmail.com>
2022-08-27 18:20:57 -0400

    feat: complete index

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,9 @@ import Link from 'next/link';
 import GoogleLogin from 'react-google-login';
 import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
 import { ImCancelCircle } from 'react-icons/im';
+import Discover from './Discover';
+import SuggestedAccounts from './SuggestedAccounts';
+import Footer from './Footer';
 
 const Sidebar = () => {
   const [showSidebar, setShowSidebar] = useState(true);
@@ -18,19 +21,21 @@ const Sidebar = () => {
     <div>
       <div
         className="block xl:hidden m-2 ml-4 mt-3 text-xl"
-        onClick={() => setShowSidebar((prev) => !prev)}
+        onClick={() => setShowSidebar(!showSidebar)}
       >
         {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
       </div>
       {showSidebar && (
-        <div className="xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
+        <div className="xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3 ">
           <div className="xl:border-b-2 border-gray-200 xl:pb-4">
             <Link href="/">
               <div className={normalLink}>
                 <p className="text-2xl">
                   <AiFillHome />
                 </p>
-                <span className="text-xl hidden xl:block">For You</span>
+                <span className="capitalize text-xl hidden xl:block">
+                  For You
+                </span>
               </div>
             </Link>
           </div>
@@ -58,6 +63,9 @@ const Sidebar = () => {
               </div>
             </div>
           )}
+          <Discover />
+          <SuggestedAccounts />
+          <Footer />
         </div>
       )}
     </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   if (isSSR) return null;
 
   return (
-    <div>
+    <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
       <Navbar />
       <div className="flex gap-6 md:gap-20">
         <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
